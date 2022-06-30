5d0f91a691e3c798cbe37117baa5a991a0c86251
A_biggart <103457162+AmberArt@users.noreply.github.com>
2022-06-29 19:39:57 -0600

    Add files via upload

---
+++ src/test/java/com/blakk_mantra_site/Mantra_api/MantraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blakk_mantra_site.Mantra_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
